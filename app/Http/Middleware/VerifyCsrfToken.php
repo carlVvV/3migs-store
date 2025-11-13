797c344aa8d1459544e8da8b7933bf26dfb7916e
3MIGs Developer <developer@3migs.com>
2025-11-14 00:30:55 +0800

    Verify csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,9 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'api/v1/psgc/search/*',
         'api/veriff-webhook',
-        '/admin/veriff/sync-status',
-        '/admin/veriff/update-status',
-        'admin/veriff/sync-status',
-        'admin/veriff/update-status',
+        'admin/veriff/*',
     ];
 }
